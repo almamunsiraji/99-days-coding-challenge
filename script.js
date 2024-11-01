549938cec417e184afc6b6b63b6f0e0c00d1883a
unknown <almamunsirajii@gmail.com>
2024-11-01 23:54:13 +0600

    1st days commit

---
+++ script.js
@@ -0,0 +1,9 @@
+import { add, sub } from "./calculator.js";
+import { person } from "./person.js";
+
+console.log(add(20, 5));
+console.log(sub(20, 5));
+console.log(person.Nationality);
+console.log(person.Name);
+console.log(person.Age);
+console.log(person.printInfo());
