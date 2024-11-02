eed721e2b70b3b85539bc30065f82559c8ff5d5e
unknown <almamunsirajii@gmail.com>
2024-11-02 23:13:43 +0600

    2nd days commit

--- script.js
+++
@@ -1,9 +0,0 @@
-import { add, sub } from "./calculator.js";
-import { person } from "./person.js";
-
-console.log(add(20, 5));
-console.log(sub(20, 5));
-console.log(person.Nationality);
-console.log(person.Name);
-console.log(person.Age);
-console.log(person.printInfo());
