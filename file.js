ce9ac45807e83097b89cad12c5f168894fd5210f
unknown <almamunsirajii@gmail.com>
2024-11-03 23:31:56 +0600

    3 days commit

--- file.js
+++ file.js
@@ -1,50 +1,89 @@
-var a = 33;
-var b = 10;
-var firstname;
-firstname = " Abdullah";
-document.write(firstname);
-var lastname;
-lastname = " Al Mamun";
-document.write(lastname);
-var linebreak;
-linebreak = " <br />";
-document.write(linebreak);
-var age;
-age = 24;
-document.write("Age: ");
-document.write(age);
-document.write(linebreak);
-result = a + b;
-document.write(" Sum: ");
-document.write(result);
-document.write(linebreak);
-result = a - b;
-document.write(" Abst: ");
-document.write(result);
-document.write(linebreak);
-result = a * b;
-document.write(" Multi: ");
-document.write(result);
-document.write(linebreak);
-result = a / b;
-document.write(" Div: ");
-document.write(result);
-document.write(linebreak);
-result = a % b;
-document.write(" Mode: ");
-document.write(result);
-document.write(linebreak);
-x = 5 + "5";
-document.write(" x= ");
-document.write(x);
-document.write(linebreak);
-document.write(
-  " Hello World! Hello Bangladesh ! \
-Hello Sirajganj"
-);
-document.write(" <br />");
-document.write('We are so-called "Vikings" from the north');
-document.write(linebreak);
-var txt = ' We are so-called "Vikings" from the north.';
-document.write(txt);
-document.write(linebreak);
+let firstNumber = Number(prompt("Enter First Number"));
+let secondNumber = Number(prompt("Enter Second Number"));
+console.log(`${firstNumber} + ${secondNumber} = ${firstNumber + secondNumber}`);
+console.log(`${firstNumber} - ${secondNumber} = ${firstNumber - secondNumber}`);
+console.log(`${firstNumber} * ${secondNumber} = ${firstNumber * secondNumber}`);
+console.log(`${firstNumber} / ${secondNumber} = ${firstNumber / secondNumber}`);
+console.log(`${firstNumber} % ${secondNumber} = ${firstNumber % secondNumber}`);
+
+/* console.log("25+20="+(25+20));
+console.log(25-20);
+console.log(25*20);
+console.log(25/20);
+console.log(25%20); */
+
+/*let price = 1500;
+discount = 100;
+console.log("Id: 1");
+console.log("title:Iphone 14");
+console.log("Description: Good Camera");
+console.log("price:" + price);
+console.log("price:" + price);
+console.log("price:" + price);
+console.log("Rating:4.98");
+console.log("Price after discount:" + (price - discount));
+price = 1200;
+console.log("price:" + price); */
+
+/* Printing describe myself */
+/* console.log(
+  ` I am Abdullah Al Mamun. \n I am 24 years old. \n I am currently living in Dhaka`
+);*/
+// console.log(1);
+// console.log("Hello");
+// console.log("Hello World!");
+// console.warn("Hello1");
+// console.error("Hello2");
+// console.clear();
+// confirm("Do you want to see this message or not");
+// alert(`hello`);
+// var a = 33;
+// var b = 10;
+// var firstname;
+// firstname = " Abdullah";
+// document.write(firstname);
+// var lastname;
+// lastname = " Al Mamun";
+// document.write(lastname);
+// var linebreak;
+// linebreak = " <br />";
+// document.write(linebreak);
+// var age;
+// age = 24;
+// document.write("Age: ");
+// document.write(age);
+// document.write(linebreak);
+// result = a + b;
+// document.write(" Sum: ");
+// document.write(result);
+// document.write(linebreak);
+// result = a - b;
+// document.write(" Abst: ");
+// document.write(result);
+// document.write(linebreak);
+// result = a * b;
+// document.write(" Multi: ");
+// document.write(result);
+// document.write(linebreak);
+// result = a / b;
+// document.write(" Div: ");
+// document.write(result);
+// document.write(linebreak);
+// result = a % b;
+// document.write(" Mode: ");
+// document.write(result);
+// document.write(linebreak);
+// x = 5 + "5";
+// document.write(" x= ");
+// document.write(x);
+// document.write(linebreak);
+// document.write(
+//   " Hello World! Hello Bangladesh ! \
+// Hello Sirajganj"
+// );
+// document.write(" <br />");
+// document.write('We are so-called "Vikings" from the north');
+// document.write(linebreak);
+// var txt = ' We are so-called "Vikings" from the north.';
+// document.write(txt);
+// document.write(linebreak);
