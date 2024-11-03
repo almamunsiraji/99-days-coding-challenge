949a33ab2b25b95d90ac266e58c47702e5181f20
unknown <almamunsirajii@gmail.com>
2024-11-03 23:44:27 +0600

    3 days commit

---
+++ file1.js
@@ -0,0 +1,40 @@
+let firstNumber = Number(prompt("Enter First Number"));
+let secondNumber = Number(prompt("Enter Second Number"));
+console.log(`${firstNumber} + ${secondNumber} = ${firstNumber + secondNumber}`);
+console.log(`${firstNumber} - ${secondNumber} = ${firstNumber - secondNumber}`);
+console.log(`${firstNumber} * ${secondNumber} = ${firstNumber * secondNumber}`);
+console.log(`${firstNumber} / ${secondNumber} = ${firstNumber / secondNumber}`);
+console.log(`${firstNumber} % ${secondNumber} = ${firstNumber % secondNumber}`);
+
+console.log("25+20=" + (25 + 20));
+console.log(25 - 20);
+console.log(25 * 20);
+console.log(25 / 20);
+console.log(25 % 20);
+
+let price = 1500;
+discount = 100;
+console.log("Id: 1");
+console.log("title:Iphone 14");
+console.log("Description: Good Camera");
+console.log("price:" + price);
+console.log("price:" + price);
+console.log("price:" + price);
+console.log("Rating:4.98");
+console.log("Price after discount:" + (price - discount));
+price = 1200;
+console.log("price:" + price);
+
+// Printing describe myself
+
+console.log(
+  ` I am Abdullah Al Mamun. \n I am 24 years old. \n I am currently living in Dhaka`
+);
+console.log(1);
+console.log("Hello");
+console.log("Hello World!");
+console.warn("Hello1");
+console.error("Hello2");
+console.clear();
+confirm("Do you want to see this message or not");
+alert(`hello`);
