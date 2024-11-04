387074d8229528e258bf94e0e7bd40b46e749e7d
unknown <almamunsirajii@gmail.com>
2024-11-04 14:18:39 +0600

    4th days commit

---
+++ file2.js
@@ -0,0 +1,68 @@
+// check largest number
+let number1 = 190;
+let number2 = 190;
+let number3 = 190;
+if (number1 > number2 && number1 > number3) {
+  console.log(`largest number is: ${number1}`);
+} else if (number2 > number1 && number2 > number3) {
+  console.log(`largest number is: ${number2}`);
+} else if (number3 > number1 && number3 > number2) {
+  console.log(`largest number is: ${number3}`);
+} else {
+  console.log(`Numbers are Equal`);
+}
+
+// check vowel and consonant
+let letter = "A";
+letter = letter.toLowerCase();
+if (
+  letter == "a" ||
+  letter == "e" ||
+  letter == "i" ||
+  letter == "o" ||
+  letter == "u"
+) {
+  console.log("vowel");
+} else {
+  console.log("consonant");
+}
+
+// marks grade point
+let marks = 30;
+if (marks < 0 || marks > 100) {
+  console.log("Invalid Marks");
+} else if (marks > 80) {
+  console.log("A+");
+} else if (marks > 33) {
+  console.log("pass");
+} else if (marks < 33) {
+  console.log("fail");
+}
+
+// check potive and negative number
+let x = -50;
+if (x > 0) {
+  console.log("positive");
+}
+if (x < 0) {
+  console.log("negative");
+}
+if (x == 0) {
+  console.log("Zero");
+}
+//  let x = 50;
+//  if (x > 0) {
+//   console.log("positive");
+//  } else if (x < 0) {
+//    console.log("negative");
+//  } else {
+//        console.log("Zero");
+//  }
+
+//  Control statement
+const number = 7;
+if (number % 2 == 0) {
+  console.log("even");
+} else {
+  console.log("odd");
+}
