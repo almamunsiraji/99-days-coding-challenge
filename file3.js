0a2463df6453a5ad106026a62de39a80bb2ba09c
unknown <almamunsirajii@gmail.com>
2024-11-05 12:21:48 +0600

    5th commit

---
+++ file3.js
@@ -0,0 +1,122 @@
+let i = 1;
+do {
+  console.log(i);
+  i++;
+} while (i <= 10);
+
+/*let i = 1;
+let count = 0;
+while (i <= 10) {
+  console.log(i);
+  i++;
+  count = count + i;
+}
+console.log(`count = ${count}`); */
+
+/*let count = 0;
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
+  count = count + i;
+}
+
+console.log(`count = ${count}`);
+*/
+/*for (let count = 1; count <= 5; count++) {
+  console.log("Bangladesh", count);
+}*/
+
+/*let letter = prompt("Enter a letter");
+letter = letter.toLowerCase();
+switch (letter) {
+  case "a":
+  case "e":
+  case "i":
+  case "o":
+  case "u": {
+    console.log("vowel");
+    break;
+  }
+  default: {
+    console.log("consonant");
+    break;
+  }
+}*/
+
+/*Weekday= sunday(1);monday(2); tuesday(3);wednesday(4); thrusday(5)
+weekend= friday(6); saturday(7); */
+/*let numberOfDay = parseInt(
+  prompt(
+    "Enter number 1 for sunday, 2 for monday, 3 for tuesday, 4 for wednesday, 5 for thrusday, 6 for friday, 7 for suturday :"
+  )
+);
+switch (numberOfDay) {
+  case 1:
+  case 2:
+  case 3:
+  case 4:
+  case 5: {
+    console.log("weekday");
+    break;
+  }
+  case 6:
+  case 7: {
+    console.log("weekend");
+    break;
+  }
+  default: {
+    console.log("invalid number");
+  }
+}*/
+
+/*switch (numberOfDay) {
+  case 1: {
+    console.log("weekday");
+    break;
+  }
+  case 2: {
+    console.log("weekday");
+    break;
+  }
+  case 3: {
+    console.log("weekday");
+    break;
+  }
+  case 4: {
+    console.log("weekday");
+    break;
+  }
+  case 5: {
+    console.log("weekday");
+    break;
+  }
+  case 6: {
+    console.log("weekend");
+    break;
+  }
+  case 7: {
+    console.log("weekend");
+    break;
+}
+default:{
+    console.log("invalid number");
+   
+}
+}*/
+
+/*if (numberOfDay == 1) {
+  console.log("weekday");
+} else if (numberOfDay == 2) {
+  console.log("weekday");
+} else if (numberOfDay == 3) {
+  console.log("weekday");
+} else if (numberOfDay == 4) {
+  console.log("weekday");
+} else if (numberOfDay == 5) {
+  console.log("weekday");
+} else if (numberOfDay == 6) {
+  console.log("weekend");
+} else if (numberOfDay == 7) {
+  console.log("weekend");
+} else {
+  console.log("invalid number");
+} */
