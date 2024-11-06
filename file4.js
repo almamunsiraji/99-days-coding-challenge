8669ad59954c7ce281ae8e6b3850e9972a72b4ec
unknown <almamunsirajii@gmail.com>
2024-11-06 22:43:56 +0600

    6th days commit

---
+++ file4.js
@@ -0,0 +1,146 @@
+// String
+const printArray = (items) => {
+  for (const item of items) {
+    console.log(item);
+  }
+};
+const greeting = "Hello World";
+console.log(greeting.length);
+console.log(greeting.charAt(1));
+console.log(greeting.indexOf("test"));
+console.log(greeting.indexOf("Hello"));
+console.log(greeting.indexOf("World"));
+console.log(greeting.lastIndexOf("o"));
+console.log(greeting.slice(2, 4));
+console.log(greeting.toLowerCase());
+console.log(greeting.toUpperCase());
+console.log(greeting.trim);
+console.log(greeting.replace("Hello", "bye"));
+const text =
+  " Hello Everyone. I am Abdullah Al Mmaun and studying in BSC in CSE";
+const spilededstring = text.split(" ");
+printArray(spilededstring);
+
+// Array
+/*const printArray = (items) => {
+  for (const item of items) {
+    console.log(item);
+  }
+};
+const fruits = ["Apple", "Banana", "Orange", "Mango", "lichi"];
+printArray(fruits);
+console.log("After pushing fruits");
+fruits.push("cherry", "strwabery");
+printArray(fruits);
+console.log("After poping fruits");
+fruits.pop();
+printArray(fruits);
+const array1 = [1, 2, 3];
+const arra2 = [4, 5, 6];
+const combined = array1.concat(arra2);
+console.log("After concatening the array");
+printArray(combined);
+const countries = ["Australia", "Pakistan", "Bangladesh", "Nepal", "India"];
+console.log("Before slicing the countries");
+printArray(countries);
+const sliced = countries.slice(1, 3);
+console.log("After slicing the array");
+printArray(sliced); */
+// Arrow Function
+/*const addNumbers = (num1, num2) => num1 + num2;
+const subNumbers = (num1, num2) => num1 + num2;
+const mulNumbers = (num1, num2) => num1 + num2;
+const modeNumbers = (num1, num2) => num1 + num2;
+console.log(addNumbers(20, 30));
+console.log(subNumbers(50, 30));
+console.log(mulNumbers(50, 40));
+console.log(modeNumbers(40, 30));
+const person = {
+  name: "Abdullah Al Mamun",
+  age: 24,
+  cgpa: 3.56,
+};
+console.log(person.name);
+console.log(person.age);
+console.log(person.cgpa);*/
+
+/*function display(message) {
+  console.log(message);
+}
+const display2 = (message) => console.log(message);
+display("Hello World");
+display2("Hellow Horld!");
+*/
+/*function display() {
+  console.log("Hello World");
+}
+const display2 = () => console.log("Hello World2!");
+display();
+display2();*/
+
+// Traditional Function
+/*function addNumbers(num1, num2) {
+  let result = num1 + num2;
+  console.log(`${num1}+${num2}=${result}`);
+}
+function subNumbers(num1, num2) {
+  let result = num1 - num2;
+  console.log(`${num1}- ${num2}=${result}`);
+}
+
+function multiNumbers(num1, num2) {
+  let result = num1 * num2;
+  console.log(`${num1}*${num2}=${result}`);
+}
+
+function divideNumbers(num1, num2) {
+  let result = num1 / num2;
+  console.log(`${num1}/${num2}=${result}`);
+}
+
+function modeNumbers(num1, num2) {
+  let result = num1 % num2;
+  console.log(`${num1}%${num2}=${result}`);
+}
+
+addNumbers(10, 20);
+addNumbers(30, 40);
+addNumbers(60, 40);
+subNumbers(50, 30);
+multiNumbers(50, 40);
+divideNumbers(500, 50);
+modeNumbers(100, 30); */
+
+/*let num1, num2, result;
+function grettings(message){
+console.log(message);
+}
+grettings("Hello Good Morning");
+grettings("Hello Good Night");
+*/
+/*function grettings() {
+  console.log("Good morning! How are you? ");
+}
+grettings();
+grettings();
+grettings();*/
+/*num1 = 50;
+num2 = 20;
+result = num1 + num2;
+console.log(`${num1}+${num2}= ${result}`);
+num1 = 50;
+num2 = 20;
+result = 50 - 20;
+console.log(`${num1}-${num2}= ${result}`);
+num1 = 50;
+num2 = 20;
+result = 50 / 20;
+console.log(`${num1}/${num2}= ${result}`);
+num1 = 50;
+num2 = 20;
+result = 50 % 20;
+console.log(`${num1}% ${num2}= ${result}`);
+num1 = 50;
+num2 = 20;
+result = 50 * 20;
+console.log(`${num1}*${num2}= ${result}`);*/
